2caf34b8e80cd3bbc0539ed93d018ec7354cc78d
Paul Rail <paul.a.rail@gmail.com>
2017-10-28 16:47:42 -0400

    Added new content for modern responsive sites
    SEO optimization, markup validation, custom media queries for var screen
    sizes, cross browser test, image optimization

---
+++ Responsive Modern Website/general instructions.js
@@ -0,0 +1,25 @@
+https://tinypng.com/  for optimizing imgages to reduce their size for web use
+
+https://validator.w3.org  for reviewing the HTML code to use best practices
+
+http://www.responsinator.com/ to test what site looks like on different screensices
+also
+http://quirktools.com/screenfly/
+
+https://www.w3schools.com/css/css_rwd_mediaqueries.asp
+RESPONSICE CSS FOR SMALLER SCREEN SIZES USING "COSTUM MEDIA QUERIES"
+
+
+<!-- For SEO Optimisation: --> //include these lines in the HTML header
+<meta name="description" contents="Simple, clean, responsive website built with HTML5, CSS3, JS and jQuery and Bootstrap 4">
+<meta name="keywords" content="web, design, html, css, html5, css3, javascript, jquery, bootstrap, development">
+
+
+/*
+Also remember to do cross browser testing for:
+-Chrome
+-Firefox
+-Opera
+-Safari
+-Edge / Internet Explorer
+*/
