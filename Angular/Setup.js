bc6c1516e6747e582eaa15b09c4cc12acde0949c
Paul Rail <paul.a.rail@gmail.com>
2017-10-30 01:31:40 -0400

    Adding content for Angular

---
+++ Angular/Setup.js
@@ -0,0 +1,10 @@
+/* Cheat-sheet to setup Angular environment:
+(as per Udemy course Angular Complete Course Beginner to Advanced)
+
+1. Create repository / folder with github desktop.
+2. Use Command Prompt (CP) to cd into that folder
+3. In CP type:  npm install -g @angular/cli
+
+
+TO CREATE A NEW ANGULAR PROJECT:
+exanple: in CP type:  ng new  hello-world
