c2de43ea5ed6e179f0a406d411cc363afc78609e
Paul Rail <paul.a.rail@gmail.com>
2017-11-03 12:54:06 -0400

    Input properties

---
+++ Angular/reusableComponent.component.ts
@@ -0,0 +1,14 @@
+//Section 5 of course
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  post = {
+    title: "Title",
+
+  }
+}
