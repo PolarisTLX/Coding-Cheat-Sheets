9389f321dcbf9f1c09466fc74708c956591c9b44
Paul Rail <paul.a.rail@gmail.com>
2017-11-10 23:58:38 -0500

    Exploring a bit of Java

---
+++ Java/Differences-Java-JavaScript.java
@@ -0,0 +1,25 @@
+/*DIFFERENCES BETWEEN JAVA AND JAVASCRIPT (JS)*/
+
+JAVA needs compiling whereas JS does not
+
+//THIS IS APPARENTLY NOT ACCURATE:
+JAVA is OOP, JavaScript is a scripting language
+
+JAVA is said to be more verbose than JS or Python
+
+JAVA can run on multiple OSes, JS cannot create standalone applications
+
+JAVA runs in a browser OR a virtual machine, JS ONLY runs in a browser
+
+
+/*HELLO WORLD IN JAVA:*/
+public class Hello
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+  }
+}
+
+/*HELLO WORLD IN JavaScript:*/
+console.log("Hello World!");
