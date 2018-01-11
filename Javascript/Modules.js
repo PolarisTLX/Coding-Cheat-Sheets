3d9388075a9d5b1525fabfe6e3c13051e061e785
Paul Rail <paul.a.rail@gmail.com>
2018-01-10 23:52:36 -0500

    Finished chapter
    Still having issues with commiting from desktop app

--- Javascript/Modules.js
+++ Javascript/Modules.js
@@ -367,3 +367,178 @@ a real AMD implementation is quite a lot more clever and robust about resolving 
 RequireJS.org provides a popular implementation of this style of module loader.
 
 //END OF COFFEE & CODE  SESSION
+
+
+INTERFACE DESIGN:
+PREDICTABILITY - COMPOSABILITY - LAYERED INTERFACES
+
+Predictability: try to follow convention so users can predict the way your interface will work
+and avoid having to spend a lot of time figuring out how it works.
+Avoid being unecessarily clever.
+
+Composability: try to use simplest data structures possible.
+Make functions do a single, clear thing only. To make them "pure" functions.
+The module should be easily composed with other code (by following common convention).
+
+Example: a spell-checker:
+
+-Option 1: If made to operate directly on whichever complicated data structures the editor uses
+and directly call internal functions in the editor.
+This module could not be used with any other programs.
+
+-Option 2: define a spell-checking interface that just requires you pass a simple string
+and it will return the position in the string where it found a possible misspelling,
+along with an array of suggested corrections.
+This interface could also be composed with other systems because strings and arrays are always available in JavaScript.
+
+
+Layered Interfaces:  You want to reduce complexity, however,
+when a user wants to do complicated things with your module, they should be able to.
+A good solution often is to offer two interfaces:
+A detailed deep-level one + a simple surface-level one.
+
+
+SUMMARY:
+Modules provide structure to larger programs by seperating the code into different files and namespaces.
+Giving these modules well-defined interfaces makes them easier to use and reuse.
+Also makes it possible to use them again later for other projects.
+
+JavaScript is not very helpful for modules unlike other languages,
+but the flexible functions and objects it provides make it possible to define nice modules.
+
+Functin scopes can be used as internal namespaces for the module.
+Objects can be used to store sets of exported values.
+
+There are two commonly-used approaches to modules in JavaScript:
+
+"CommonJS Modules":
+-revolves around a "require" functin that fetches a module by name and returns its interface.
+
+AMD (ASYNCHRONOUS MODULE DEFINITION):
+- uses a "define" functin that takes an array of module names and a functin,
+after loading the modules, runs the functins with their interfaces as arguments.
+This code is a lot harder to follow than the "require" functin and does follow a simple, predictable path.
+
+
+
+EXERCISES:
+
+MONTHS NAMES:
+
+Write a simple module system similar to the weekDay module that can convert month numbers (zero-based, as in the Date type),
+to names and can convert names back to numbers.
+Give it its own namespace since it will need an internal array of month names.
+Use plain JavaScript, without any module loader systems.
+
+
+Recall that months in the Date type in JS are quirky, in that they are numbered 0-11.
+
+var month = function() {
+  var names = ["January", "February", "March", "April", "May", "June", "July",
+               "August", "September", "October", "November", "December"];
+  return {
+    name: function(number) { return names[number];},
+    number: function(name) { return names.indexOf(name);}
+  };
+// };
+}();   //NOTE  Needs the final () brackets (immeditaly invoked) or it will not work.
+
+TESTS:
+console.log(month.name(2));   // March
+console.log(month.number("November"));  //10
+
+
+Solution structure:
+
+// a function is expressed:
+// the variable name "month" is what stores the interface,
+// as in   month.name(2),  or month.number("November").
+var month = function() {
+  // wraps the variable that holds the array of data needed:
+  var names = ["January", "February", "March", "April", "May", "June", "July",
+               "August", "September", "October", "November", "December"];
+
+  // returns an object that holds both functions so they can be exported:
+  return {
+    name: function(number) { return names[number];},
+    number: function(name) { return names.indexOf(name);}
+  };
+  // immidiately invoked
+}();
+
+
+
+A RETURN TO ELECTRONIC LIFE:
+Converting that project into a version that is seperated into modules.
+
+You dont want a module for each functin and type.
+Just like a book does not make each page a new chapter.
+Instead choose which functions and types you want to group together.
+
+The size of the project will also determine how many modules are a good number.
+Dont want to open 10 files for a tiny project.
+
+You can choose to have some functions become internal to their module,
+and thus inaccessible to other modules.
+
+
+Recommended grouping:
+
+Module "grid":
+  -Vector
+  -Grid
+  -directions
+  -directionNames
+
+Module "World":
+  -(randomElement)
+  -(elementFromChar)
+  -(charFromElement)
+  -View
+  -World
+  -LifelikeWorld
+  -directions [reexported]
+
+Module "simple_ecosystem"
+  (randomElement) [duplicated]
+  (dirPlus)
+  Wall
+  BouncingCritter
+  WallFollower
+
+Module "ecosystem"
+  Wall [duplicated]
+  Plant
+  PlantEater
+  SmartPlantEater
+  Tiger
+
+
+Reexported the "directions" array from the "grid" module from "world",
+so that modules built on that (the ecosystems) don’t have to know or worry about the existence of the grid module.
+
+Also duplicated two generic and tiny helper values ("randomElement" and "Wall"),
+since they are used as internal details in different contexts,
+and do not belong in the interfaces for these modules.
+
+
+
+CIRCULAR DEPENDENCIES:
+
+Most module system prohibit a scenario where Module A depends on Module B, And Module B also depdends back on Module A.
+
+CommonJS modules allow a limited form:
+as long as the modules do not replace their default "exports" object with another value,
+and start accessing each others interfaces ONLY after they finish loading.
+
+A way in which this feature could be implemented:
+The trick is to add the "exports" object of a module to "require"s cache BEFORE running the module.
+This prevents the module from being able to override module.exports as it doesnt have a chance to do so.
+so we do not know whether it may want to export some other value.
+After loading, the cache object is overridden with module.exports, which may be a different value.
+
+But if in the course of loading the module, a second module is loaded that asks for the first module,
+its default "exports" object, which is likely still empty by this point, will be in the cache,
+and the second module will receive a reference to it.
+If it doesnt try to do anything with the object until the first module has finished loading,
+things will work.
