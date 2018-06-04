8a3deae60a6a854cc44ace7d780d57603d0c9099
PolarisTLX <paul.a.rail@gmail.com>
2018-06-03 23:23:11 -0400

    More .filter exercise practice

--- Javascript/Useful Algorithms/Especially Useful Algorithms/Filter.js
+++ Javascript/Useful Algorithms/Especially Useful Algorithms/Filter.js
@@ -25,3 +25,35 @@ and its return value determines whether an element is included in the returned a
 Note how the filter function, rather than deleting elements from the existing array,
 builds up a new array with only the elements that pass the test.
 This function is "PURE". It does not modify the array it is given.
+
+
+
+.filter   WITH   .map exercise:
+https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/es6/write-higher-order-arrow-functions/
+
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr.filter((element) => {
+    if (element > 0 && Number.isInteger(element)) {
+      return element;
+    }
+   }).map((element) => element**2);
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+
+
+Tests:
+-User did replace var keyword.
+-squaredIntegers should be a constant variable (by using const).
+-squaredIntegers should be an array
+-squaredIntegers should be [16, 1764, 36]
+-function keyword was not used.
+-loop should not be used
+-map, filter, or reduce should be used
